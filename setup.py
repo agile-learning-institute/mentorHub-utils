7b2367b420c5446e9c81fbca61db1b60837f6807
Michael Quinn <153249112+michquinn@users.noreply.github.com>
2025-07-30 21:26:13 -0400

    Update version and changelog

--- setup.py
+++ setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name='mentorhub-utils',
-    version='1.0.3',
+    version='1.1.0',
     description='Helper utilities for Mentorhub API projects',
     long_description=long_description,
     long_description_content_type='text/markdown',
